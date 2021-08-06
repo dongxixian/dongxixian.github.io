b745ee2e99fd0662f2f605a2b73a5844a31e9b66
dongxixian <2968735791@qq.com>
2021-08-06 15:58:13 +0800

    Site updated: 2021-08-06 15:58:13

--- js/xinxin.js
+++ js/xinxin.js
@@ -1,7 +1,7 @@
-// 设置页脚博主会动的心
+/*/ 设置页脚博主会动的心
 $(document).ready(function(e){
     $('.copyright').html('2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Glerdox');
-})
+})*/
 
 
 // 移除页脚样式
