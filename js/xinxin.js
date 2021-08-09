17ea4859f4ba598e0465111b32a28a2b8ca4eb10
dongxixian <2968735791@qq.com>
2021-08-09 16:07:04 +0800

    Site updated: 2021-08-09 16:07:03

--- js/xinxin.js
+++ js/xinxin.js
@@ -4,9 +4,10 @@ $(document).ready(function(e){
 })*/
 
 
-// 移除页脚样式
+/*// 移除页脚样式
 var app = document.getElementById('footer');
 app.removeAttribute('id')
+*/
 
 /*添加图片top*/
 var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
